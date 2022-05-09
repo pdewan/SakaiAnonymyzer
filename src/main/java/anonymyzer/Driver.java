914014384279499b98576360f031dfa4b561f4fe
Ken Liu <yiwk321@live.unc.edu>
2022-05-09 14:58:25 -0400

    add driver

---
+++ src/main/java/anonymyzer/Driver.java
@@ -0,0 +1,12 @@
+package anonymyzer;
+
+import java.io.IOException;
+
+public class Driver {	
+	static String[] arg = {"at", 
+						   "F:\\Hermes Data\\Assignment0.zip"};
+
+	public static void main(String[] args) throws IOException {
+		AnonFaker.main(arg);
+	}
+}
